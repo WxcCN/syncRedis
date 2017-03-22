f0457c4d7b82e637b5086eac74f7e2c17285eb0a
nova <357991863@qq.com>
2017-03-22 19:32:34 +0800

    change1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,6 +21,7 @@ public class Main {
         public static void main(String[] args) {
             //Gamma g1 = new Alpha();
             Gamma g2 = new Gamma();
+            System.out.println();
             System.out.println(  " "
                     + g2.getType());
         }
